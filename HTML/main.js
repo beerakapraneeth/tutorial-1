a968db74dfa1291d64b3e043356bff2b58a61734
beerakapraneeth <108787673+beerakapraneeth@users.noreply.github.com>
2022-07-10 23:35:53 +0530

    Add files via upload

---
+++ HTML/main.js
@@ -0,0 +1,6 @@
+function print(){
+    console.log(document.getElementById("t1").value);
+    console.log(document.getElementById("t2").value);
+    console.log(document.getElementById("t3").value);
+    console.log(document.getElementById("t4").value);
+}
